287b7fa6a1248e03f27c5baf852554e1210a66ff
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-03 12:35:28 +0200

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Y K on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
