ea516ae96976771f192797c57085f06e8403ab7b
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-03 14:25:13 +0200

    Order in which we apply modifiers to view matters since that influnce the View itself

--- SwiftUIBasics/ImagesSample.swift
+++ SwiftUIBasics/ImagesSample.swift
@@ -3,13 +3,22 @@
 //  SwiftUIBasics
 //
 //  Created by Y K on 03.11.2023.
-//
+
+//Order in which we apple modifiers to view amtters since that influnce the View itself
 
 import SwiftUI
 
 struct ImagesSample: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "person.circle")
+            .resizable()
+//            .font(.system(size: 200))
+//            .imageScale(.large)
+            .foregroundColor(.colorScheme)
+            .scaledToFill()
+            .frame(width: 200, height: 100)
+            .clipped()
+            
     }
 }
 
