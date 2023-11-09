8816725a4b786a181b361def651ce11168f93f48
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-10 01:08:40 +0200

    pushed

--- SwiftUIBasics/ControlFlowSample.swift
+++ SwiftUIBasics/ControlFlowSample.swift
@@ -9,32 +9,41 @@ import SwiftUI
 
 struct ControlFlowSample: View {
     @State private var darkModeEnab = false
+    @State private var notificationEnabled = false
+    @State private var faceIDEnabled = false
+
+
     
     var backgroundColor: Color {
         // conditional computed property
-//        if darkModeEnab {
-//            return .black
-//        } else {
-//            return .white
-//        }
+        //        if darkModeEnab {
+        //            return .black
+        //        } else {
+        //            return .white
+        //        }
         // ternary operator
         return darkModeEnab ? .black : .white
     }
+    
+    var rowBackgroundColor: Color {
+        return darkModeEnab ? Color.white.opacity(0.1) : Color(.systemGroupedBackground)
+        
+    }
     var body: some View {
         VStack(alignment: .leading) {
-//            if darkModeEnab {
-//                Text("Settings")
-//                    .font(.largeTitle)
-//                    .fontWeight(.semibold)
-//                    .foregroundStyle(.white)
-//                    .padding()
-//            } else {
-//                Text("Settings")
-//                    .font(.largeTitle)
-//                    .fontWeight(.semibold)
-//                    .foregroundStyle(.black)
-//                    .padding()
-//            }
+            //            if darkModeEnab {
+            //                Text("Settings")
+            //                    .font(.largeTitle)
+            //                    .fontWeight(.semibold)
+            //                    .foregroundStyle(.white)
+            //                    .padding()
+            //            } else {
+            //                Text("Settings")
+            //                    .font(.largeTitle)
+            //                    .fontWeight(.semibold)
+            //                    .foregroundStyle(.black)
+            //                    .padding()
+            //            }
             Text("Settings")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
@@ -45,23 +54,90 @@ struct ControlFlowSample: View {
                 Text("Appearance")
                     .font(.headline)
                     .foregroundStyle(darkModeEnab ? Color.white : Color.black)
-               
-                Toggle(isOn: $darkModeEnab) {
-                    Image(systemName: "moon.circle.fill")
-                        .font(.title2)
-                        .imageScale(.medium)
-                        
-                    
-                    Text("Dark Mode Enabled")
-                        .font(.subheadline)
-                        
                 
-                    
+                Toggle(isOn: $darkModeEnab) {
+                    HStack {
+                        Image(systemName: "moon.circle.fill")
+                            .font(.title2)
+                            .imageScale(.medium)
+                            .foregroundStyle(.blue)
+                        
+                        
+                        
+                        Text("Dark Mode Enabled")
+                            .font(.subheadline)
+                            .foregroundStyle(darkModeEnab ? Color.white : Color.black)
+                        
+                        
+                    }
                 }
                 .padding(.horizontal)
                 .frame(height: 44)
-                .background(Color(.systemGroupedBackground))
+                .background(rowBackgroundColor)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
+                .toggleStyle(SwitchToggleStyle(tint: .blue))
+            }
+            .padding()
+            
+            VStack(alignment: .leading) {
+                Text("Account")
+                    .font(.headline)
+                    .foregroundStyle(darkModeEnab ? Color.white : Color.black)
+                
+                VStack {
+                    Toggle(isOn: $notificationEnabled) {
+                        HStack {
+                            Image(systemName: "bell.circle.fill")
+                                .font(.title2)
+                                .imageScale(.medium)
+                                .foregroundStyle(.blue)
+                            
+                            
+                            
+                            VStack(alignment: .leading) {
+                                Text("Notifications")
+                                    .font(.subheadline)
+                                    .foregroundStyle(darkModeEnab ? Color.white : Color.black)
+                                
+                                if notificationEnabled {
+                                    Text("Push Notifications enabled")
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                }
+                            }
+                            
+                        }
+                        
+                    }
+                    .padding(.horizontal)
+                    .frame(height: 44)
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    
+                    Divider()
+                    
+                    Toggle(isOn: $faceIDEnabled) {
+                        HStack {
+                            Image(systemName: "faceid")
+                                .font(.title2)
+                                .imageScale(.medium)
+                                .foregroundStyle(.blue)
+                            
+                            
+                            
+                            Text("Enable Face ID")
+                                .font(.subheadline)
+                                .foregroundStyle(darkModeEnab ? Color.white : Color.black)
+                            
+                            
+                        }
+                    }
+                    .padding(.horizontal)
+                    .frame(height: 44)
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                }
+                .background(rowBackgroundColor)       .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                
             }
             .padding()
             
