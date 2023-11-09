e013bcb0f707b5106f9ac82ec62c1f04964c1afc
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-10 00:24:43 +0200

    A great example and comparison of ternary operator and if-else statement                 Text("Settings")                     .font(.largeTitle)                     .fontWeight(.semibold)                     .foregroundStyle(.white) or .foregroundColor(darkModeEnab ? Color.white : Color.black)                     .padding()

--- SwiftUIBasics/ControlFlowSample.swift
+++ SwiftUIBasics/ControlFlowSample.swift
@@ -9,21 +9,48 @@ import SwiftUI
 
 struct ControlFlowSample: View {
     @State private var darkModeEnab = false
+    
+    var backgroundColor: Color {
+        // conditional computed property
+//        if darkModeEnab {
+//            return .black
+//        } else {
+//            return .white
+//        }
+        // ternary operator
+        return darkModeEnab ? .black : .white
+    }
     var body: some View {
         VStack(alignment: .leading) {
+//            if darkModeEnab {
+//                Text("Settings")
+//                    .font(.largeTitle)
+//                    .fontWeight(.semibold)
+//                    .foregroundStyle(.white)
+//                    .padding()
+//            } else {
+//                Text("Settings")
+//                    .font(.largeTitle)
+//                    .fontWeight(.semibold)
+//                    .foregroundStyle(.black)
+//                    .padding()
+//            }
             Text("Settings")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
+                .foregroundStyle(darkModeEnab ? Color.white : Color.black)
                 .padding()
             
             VStack(alignment: .leading) {
                 Text("Appearance")
                     .font(.headline)
+                    .foregroundStyle(darkModeEnab ? Color.white : Color.black)
                
                 Toggle(isOn: $darkModeEnab) {
                     Image(systemName: "moon.circle.fill")
                         .font(.title2)
                         .imageScale(.medium)
+                        
                     
                     Text("Dark Mode Enabled")
                         .font(.subheadline)
@@ -40,7 +67,7 @@ struct ControlFlowSample: View {
             
             Spacer()
         }
-        .background(Color.cyan)
+        .background(backgroundColor)
     }
 }
 
